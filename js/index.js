028eb27fda796c528baa393350663ee3c1c05b2c
Антон Богаевский <anton.bahayeuski@epolsoft.com>
2019-11-27 17:53:00 +0300

    Add assist information

--- js/index.js
+++ js/index.js
@@ -320,6 +320,17 @@ $(document).ready(function () {
         console.log('error loading ' + element.data('src'));
     }
     });
-  })
+  });
+
+  $('a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top - 100,
+      },
+        500,
+        "linear"
+      );
+  });
 
 });
